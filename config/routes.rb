932ea12f9724ef89a9f9a4f4ba8e91c105db6e2d
Austin Cummings <aac729@live.com>
2020-10-05 17:25:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :posts, only: %i[show index new edit]
   end
   
-  scope '/admin' do 
+  namespace :admin do 
     resources :stats, only: [:index]
   end 
 
